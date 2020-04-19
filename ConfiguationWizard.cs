157d39802c7098e93868d6819c60ec5418fc277b
Christoph Fuchs <fuschisworld@hotmail.de>
2020-04-19 10:40:38 +0200

    Init Project

---
+++ ConfiguationWizard.cs
@@ -0,0 +1,67 @@
+﻿using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TemplateWizard;
+using System.Windows.Forms;
+
+namespace MicroserviceSolution
+{ 
+    public sealed class ConfigurationWizard : IWizard
+    {
+        private WizardInputForm inputForm;
+        private string domainName;
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        // This method is only called for item templates,
+        // not for project templates.
+        public void ProjectItemFinishedGenerating(ProjectItem
+            projectItem)
+        {
+        }
+
+        // This method is called after the project is created.
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject,
+            Dictionary<string, string> replacementsDictionary,
+            WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                // Display a form to the user. The form collects
+                // input for the custom message.
+                inputForm = new WizardInputForm();
+                inputForm.ShowDialog();
+
+                domainName = inputForm.InputMessage;
+
+                // Add custom parameters.
+                replacementsDictionary.Add("$domainname$",
+                    domainName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // This method is only called for item templates,
+        // not for project templates.
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
---
+++ WizardInputForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MicroserviceSolution
+{
+    public partial class WizardInputForm : Form
+    {
+        public WizardInputForm()
+        {
+            InitializeComponent();
+        }
+
+        public string InputMessage { get; set; } = "MyDomain";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InputMessage = this.textBox1.Text;
+        }
+    }
+}
